3d41d054c54daaa4cf782b6dc00200c6e2d8dd0e
Averance-EH <nikitafadeenko@MacBook-Pro-Nikita.local>
2020-07-08 23:17:25 +0300

    First files uploaded

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose any language available by using correct parameter")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+
+    language = request.config.getoption("language")
+    # browser = None
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    print('\nstart browser..')
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+    
